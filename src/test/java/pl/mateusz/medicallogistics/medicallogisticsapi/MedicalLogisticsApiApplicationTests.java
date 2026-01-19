3dd3d8ba33130b1403a359a23295a30bdb626619
Mateusz Rojek <mateuszr2047@gmail.com>
2026-01-19 21:52:08 +0100

    Initial commit

---
+++ src/test/java/pl/mateusz/medicallogistics/medicallogisticsapi/MedicalLogisticsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mateusz.medicallogistics.medicallogisticsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalLogisticsApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
